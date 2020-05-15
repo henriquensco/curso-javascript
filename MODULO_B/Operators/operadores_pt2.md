d3379eb36066778c5808c89d80ac5f53e182ed0b
HENRIQUE <henriquemicroweb@gmail.com>
2020-05-15 14:58:08 -0300

    nao perder os arquivos : )

---
+++ MODULO_B/Operators/operadores_pt2.md
@@ -0,0 +1,16 @@
+#OPERADORES RELACIONAIS
+
+- 5 > 2 -> TRUE
+- 7 < 4 -> FALSE
+- 8 >= 8 -> TRUE
+- 9 <= 7 -> FALSE
+- 5 == 5 -> TRUE
+- 4 != 4 -> FALSE
+
+#IDENTIDADE
+
+5 == 5 -> true
+5 == '5' -> true
+5 === '5' -> false  verifica se os valores são identicos
+5 === 5 -> true
+5 !== '5' -> true //desigual restrito *tipos diferentes*
---
+++ MODULO_B/Operators/terminal_node_pt2.js
@@ -0,0 +1,18 @@
+5 > 2 //return true
+
+8 < 4 //return false
+
+var a = 8
+var b = 15
+
+a > b //return false
+
+a < b //return true
+
+a <= b - 10 //return false
+
+//exemplos
+preco >= 200.50 // o preço é maior ou igual a 200.50?
+idade < 18 // a idade é menor do que 18?
+curso == 'JavaScript' // o curso é JavaScript?
+n1 != n2 // o primeiro número é diferente do segundo?
