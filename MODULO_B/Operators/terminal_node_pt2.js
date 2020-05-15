026161555063eb5dd1e8488754ec8beee21524fe
HENRIQUE <nascimentocostahenrique@gmail.com>
2020-05-15 15:07:01 -0300

    teste de contribuicao

--- MODULO_B/Operators/terminal_node_pt2.js
+++ MODULO_B/Operators/terminal_node_pt2.js
@@ -16,3 +16,4 @@ preco >= 200.50 // o preço é maior ou igual a 200.50?
 idade < 18 // a idade é menor do que 18?
 curso == 'JavaScript' // o curso é JavaScript?
 n1 != n2 // o primeiro número é diferente do segundo?
+
