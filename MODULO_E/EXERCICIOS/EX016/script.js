f249f31fa86d563dcc671cd2fb562f8fc93898c2
HENRIQUE <nascimentocostahenrique@gmail.com>
2020-05-31 18:57:10 -0300

    Exercicio de contagem

--- MODULO_E/EXERCICIOS/EX016/script.js
+++ MODULO_E/EXERCICIOS/EX016/script.js
@@ -1,1 +1,33 @@
-alert('bem vindo!')
+function contar() {
+  let inicio = document.getElementById('txti')
+  let fim = document.getElementById('txtf')
+  let passo = document.getElementById('txtp')
+  let res = document.getElementById('res')
+
+  if (inicio.value.length == 0 || fim.value.length == 0 || passo.value.length == 0) {
+    res.innerHTML = 'Impossível contar.'
+  } else {
+    res.innerHTML = 'Contando: '
+    let i = Number(inicio.value)
+    let f = Number(fim.value)
+    let p = Number(passo.value)
+
+    if(passo.value <= 0) {
+      window.alert('Passo inválido!')
+      p = 1
+    }
+
+    if(i < f) {
+      // Contagem crescente
+      for(let c = i; c <= f; c += p) {
+        res.innerHTML += `${c} \u{1F449}`
+      }
+    } else {
+      // Contagem regressiva
+      for(let c = i; c >= f; c -= p) {
+        res.innerHTML += `${c} \u{1F449}`
+      }
+    }
+    res.innerHTML += `\u{1F3C1}`
+  }
+}
