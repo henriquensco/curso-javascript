f3fc843f7bf485cccc15756f0e52154697932f6b
HENRIQUE <nascimentocostahenrique@gmail.com>
2020-06-03 18:04:39 -0300

    Primeira aula do MODULO F: Variaveis compostas - Arrays

---
+++ MODULO_F/AULA 15/ambiente.js
@@ -0,0 +1,13 @@
+let num = [5, 8, 2, 9, 3]
+
+//num[3] = 6
+//num.push(7) para colocar o índice na última posição
+num.push(7)
+
+//num.length saber o tamanho do array
+//num.sort() para pegar todos os números e colocar em ordem crescente
+
+console.log(`Nosso vetor é o ${num}`)
+console.log(`O vetor tem ${num.length} posições`)
+console.log(`O vetor em ordem crescente é: ${num.sort()}`)
+console.log(num[0]) // num[1] num[2] ...
